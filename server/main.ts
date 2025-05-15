63f6855bac67441e069bd8451aa7253016ee3fdf
digital-andrea <114409147+digital-andrea@users.noreply.github.com>
2025-05-15 15:38:02 +0200

    Add CSRF protection

--- server/main.ts
+++ server/main.ts
@@ -1,7 +1,9 @@
 import { Hono } from "hono";
 import { serveStatic } from "hono/deno";
+import { csrf } from "hono/csrf";
 
 const app = new Hono();
+app.use(csrf());
 
 app.get("/*", serveStatic({ root: "./client" }));
 
