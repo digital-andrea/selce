a7de98a3088f3ae682f996de775afb9bd21c844a
digital-andrea <114409147+digital-andrea@users.noreply.github.com>
2025-05-15 15:38:25 +0200

    Add example api route

--- server/main.ts
+++ server/main.ts
@@ -5,6 +5,21 @@ import { csrf } from "hono/csrf";
 const app = new Hono();
 app.use(csrf());
 
+const test = {
+  menu: {
+    id: "file",
+    value: "File",
+    popup: {
+      menuitem: [
+        { value: "New", onclick: "CreateNewDoc()" },
+        { value: "Open", onclick: "OpenDoc()" },
+        { value: "Close", onclick: "CloseDoc()" },
+      ],
+    },
+  },
+};
+
+app.get("/api/test", (c) => c.json(test));
 app.get("/*", serveStatic({ root: "./client" }));
 
 Deno.serve(app.fetch);
