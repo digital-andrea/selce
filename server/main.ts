6b29df0fb9a1b7a06148610ff3716e0a5bcae268
digital-andrea <114409147+digital-andrea@users.noreply.github.com>
2025-05-15 14:25:56 +0200

    Change docker context, have hono serve client dir

--- server/main.ts
+++ server/main.ts
@@ -1,9 +1,8 @@
 import { Hono } from "hono";
+import { serveStatic } from "hono/deno";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
+app.get("/*", serveStatic({ root: "./client" }));
 
 Deno.serve(app.fetch);
